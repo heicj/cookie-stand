612f64dd63d6c1c04f04500ba2a9be2e2b25017b
Charlie Heiner <heinercj@gmail.com>
2017-10-30 16:09:26 -0700

    added time array and get random customer per hour

--- salmonCookies.js
+++ salmonCookies.js
@@ -1,16 +1,15 @@
-// 'use strict';
+'use strict';
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomIntInclusive(min, max) {
-    min = Math.ceil(this.min);
-    max = Math.floor(this.max);
+    min = Math.ceil(min);
+    max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
 
-function hourlyTotal(perHour,cookie) {
-    perHour = this.custPerHour;
-    cookie = this.avgCookie;
-    return (perHour * cookie);
+function hrTotal (avgCookie, custPerHour) {
+    return avgCookie * custPerHour
 }
+
 
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
@@ -22,11 +21,15 @@ const pdxAirport = {
     max: 65,
     avgCookie: 6.3,
     custPerHour: getRandomIntInclusive,
-    hourlyTotal: hourlyTotal
+    hourTotal: hrTotal
+    
 }
+let numPerHour = pdxAirport.custPerHour(pdxAirport.min,pdxAirport.max);
+let hourTotal = Math.round(pdxAirport.avgCookie * numPerHour);
 
-// console.log(pdxAirport.hourlyTotal());
-console.log(pdxAirport.custPerHour());
+console.log('this is hourTotal ' + hourTotal);
+console.log('this is numperhour var ' + numPerHour);
+console.log(pdxAirport.custPerHour(pdxAirport.min,pdxAirport.max));
 
 const pioneerSquare = {
     min: 3,
@@ -34,7 +37,8 @@ const pioneerSquare = {
     avgCookie: 1.2,
     custPerHour: getRandomIntInclusive,
 }
-console.log(pioneerSquare.custPerHour());
+
+console.log(pioneerSquare.custPerHour(pioneerSquare.min,pioneerSquaree.max));
 
 const powell = {
     min: 11,
@@ -42,6 +46,8 @@ const powell = {
     avgCookie: 3.7,
     custPerHour: getRandomIntInclusive
 }
+
+console.log(pioneerSquare.custPerHour(powell.min,powell.max));
 
 
 const stJohn = {
