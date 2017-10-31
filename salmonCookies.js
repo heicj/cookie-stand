357fd39d7587235cd3d21eb1bbf4e7adc35f9bba
Charlie Heiner <heinercj@gmail.com>
2017-10-30 17:31:25 -0700

    created function to create array with hour and sales data

--- salmonCookies.js
+++ salmonCookies.js
@@ -6,14 +6,24 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
 
-function hrTotal (avgCookie, custPerHour) {
-    return avgCookie * custPerHour
+function hourSale () {
+    const salesByHour = [];
+    for (let i = 0; i < hours.length; i++ ) {
+        const hour = hours[i];
+        const custPerHour = getRandomIntInclusive(this.min,this.max);
+        const numCookies = custPerHour * this.avgCookie;
+        const oneHour = {
+            hour: hour,
+            cookiesSold: numCookies
+        }
+        salesByHour.push(oneHour);
+    }
+    this.salesByHour = salesByHour;
+
 }
 
 
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-
-
 
 
 const pdxAirport = {
@@ -21,40 +31,45 @@ const pdxAirport = {
     max: 65,
     avgCookie: 6.3,
     custPerHour: getRandomIntInclusive,
-    hourTotal: hrTotal
+    hourSale: hourSale
     
 }
-let numPerHour = pdxAirport.custPerHour(pdxAirport.min,pdxAirport.max);
-let hourTotal = Math.round(pdxAirport.avgCookie * numPerHour);
 
-console.log('this is hourTotal ' + hourTotal);
-console.log('this is numperhour var ' + numPerHour);
-console.log(pdxAirport.custPerHour(pdxAirport.min,pdxAirport.max));
+pdxAirport.hourSale();
+console.log(pdxAirport);
+
+
+// console.log('this is hourTotal ' + hourTotal);
+// console.log('this is numperhour var ' + numPerHour);
+// console.log(pdxAirport.custPerHour(pdxAirport.min,pdxAirport.max));
 
 const pioneerSquare = {
     min: 3,
     max: 24,
     avgCookie: 1.2,
     custPerHour: getRandomIntInclusive,
+    hourSale: hourSale
 }
 
-console.log(pioneerSquare.custPerHour(pioneerSquare.min,pioneerSquaree.max));
+// console.log(pioneerSquare.custPerHour(pioneerSquare.min,pioneerSquaree.max));
 
 const powell = {
     min: 11,
     max: 38,
     avgCookie: 3.7,
-    custPerHour: getRandomIntInclusive
+    custPerHour: getRandomIntInclusive,
+    hourSale: hourSale
 }
 
-console.log(pioneerSquare.custPerHour(powell.min,powell.max));
+// console.log(pioneerSquare.custPerHour(powell.min,powell.max));
 
 
 const stJohn = {
     min: 20,
     max: 38,
     avgCookie: 2.3,
-    custPerHour: getRandomIntInclusive
+    custPerHour: getRandomIntInclusive,
+    hourSale: hourSale
 }
 
 
@@ -62,7 +77,8 @@ const waterfront = {
     min: 2,
     max: 16,
     avgCookie: 4.6,
-    custPerHour: getRandomIntInclusive
+    custPerHour: getRandomIntInclusive,
+    hourSale: hourSale
 }
 
-console.log('this is waterfront custperHour ' + waterfront.custPerHour())
+// console.log('this is waterfront custperHour ' + waterfront.custPerHour())
