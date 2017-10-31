4b4089e8d4f7e56f5f7191d0f3cdd17e0b6f953a
Charlie Heiner <heinercj@gmail.com>
2017-10-30 21:54:39 -0700

    created for loop that creates li for one object

--- salmonCookies.js
+++ salmonCookies.js
@@ -1,5 +1,10 @@
 'use strict';
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+const stores = ['pdxAirport', 'pioneerSquare', 'powell', 'stJohn', 'waterfront'];
+
+
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -11,7 +16,7 @@ function hourSale () {
     for (let i = 0; i < hours.length; i++ ) {
         const hour = hours[i];
         const custPerHour = getRandomIntInclusive(this.min,this.max);
-        const numCookies = custPerHour * this.avgCookie;
+        const numCookies = Math.round(custPerHour * this.avgCookie);
         const oneHour = {
             hour: hour,
             cookiesSold: numCookies
@@ -19,39 +24,59 @@ function hourSale () {
         salesByHour.push(oneHour);
     }
     this.salesByHour = salesByHour;
-
-}
-
-function render () {
-    const ul = document.getElementById('ul');
-    const li = document.createElement('li');
-    li.textContent = 'test';
-    ul.appendChild(li);
 }
 
 
 
-const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+
+
+
+
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const ul = document.createElement('ul');
+
+// for (let i = 0; i < fruits.length; i++) {
+//     const li = document.createElement('li');
+//     li.textContent = fruits[i];
+//     ul.appendChild(li);
+// }
+
+// // add the ul to the dom...
+// document.body.appendChild(ul);
+
+
 
 
 const pdxAirport = {
+    name: 'PDX Airport',
     min: 23,
     max: 65,
     avgCookie: 6.3,
     custPerHour: getRandomIntInclusive,
-    hourSale: hourSale,
-    render: render
+    salesByHour: hourSale,
+    // render: render
+    
+
 }
-pdxAirport.render();
-pdxAirport.hourSale();
+// pdxAirport.render();
+pdxAirport.salesByHour();
+
 console.log(pdxAirport);
+console.log(pdxAirport.salesByHour[1]);
 
+const list = document.getElementById('ul');
+for( let i = 0; i < pdxAirport.salesByHour.length; i++) {
+    const li = document.createElement('li');
+    li.textContent = pdxAirport.salesByHour[i].hour + ' ' + pdxAirport.salesByHour[i].cookiesSold
+    list.appendChild(li);
+}
 
-// console.log('this is hourTotal ' + hourTotal);
-// console.log('this is numperhour var ' + numPerHour);
-// console.log(pdxAirport.custPerHour(pdxAirport.min,pdxAirport.max));
 
 const pioneerSquare = {
+    name: 'Pioneer Square',
     min: 3,
     max: 24,
     avgCookie: 1.2,
@@ -62,6 +87,7 @@ const pioneerSquare = {
 // console.log(pioneerSquare.custPerHour(pioneerSquare.min,pioneerSquaree.max));
 
 const powell = {
+    name: 'Powell\'s',
     min: 11,
     max: 38,
     avgCookie: 3.7,
@@ -73,6 +99,7 @@ const powell = {
 
 
 const stJohn = {
+    name: 'St John',
     min: 20,
     max: 38,
     avgCookie: 2.3,
@@ -82,6 +109,7 @@ const stJohn = {
 
 
 const waterfront = {
+    name: 'Waterfront',
     min: 2,
     max: 16,
     avgCookie: 4.6,
@@ -90,3 +118,13 @@ const waterfront = {
 }
 
 // console.log('this is waterfront custperHour ' + waterfront.custPerHour())
+
+// const cars = [pinto, tesla, gremlin];
+
+// const ul = document.getElementById('car-list');
+
+// for(let i = 0; i < cars.length; i++) {
+//     const li = cars[i].render();
+//     ul.appendChild(li);
+// }
+
